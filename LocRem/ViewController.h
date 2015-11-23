fd1a355311e2cc90767b4f83e80af0bc7a6cb254
Cisco <franciscoraglandjr@gmail.com>
2015-11-23 13:09:37 -0800

    Initial Commit

---
+++ LocRem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocRem
+//
+//  Created by Francisco Ragland Jr on 11/23/15.
+//  Copyright © 2015 Francisco Ragland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocRem/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LocRem
+//
+//  Created by Francisco Ragland Jr on 11/23/15.
+//  Copyright © 2015 Francisco Ragland. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
